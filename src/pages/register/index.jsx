ac33b11c0999a5171e418c0199fd1c5cb5e12235
jaimesHub <duongquockhanh230596@gmail.com>
2024-09-13 12:47:39 +0700

    12.2 - Register form - Get & Validate input data

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,18 +1,28 @@
 import { Button, Divider, Form, Input } from "antd";
 
 const Register = () => {
-    const onFinish = () => {
-        alert("Finish");
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log(">>> values: ", values);
+        form.resetFields();
     }
     return (
         <div className="register-page" style={{ padding: '50px' }}>
             <h3 style={{ textAlign: "center" }}>Register Page</h3>
             <Divider />
             <Form
+                form={form}
                 name="register"
                 labelCol={{ span: 6 }}
                 style={{ maxWidth: 600, margin: "0 auto" }}
-                onFinish={onFinish}
+                initialValues={{
+                    fullName: "",
+                    email: "",
+                    password: "",
+                    phone: "",
+                }}
+                onFinish={(e) => onFinish(e)}
             >
                 <Form.Item
                     name="fullName"
